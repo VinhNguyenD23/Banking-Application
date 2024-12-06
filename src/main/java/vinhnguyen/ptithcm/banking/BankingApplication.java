2b371e5affa1d251e7c4511338cfc289ea99449b
VinhNguyenD23 <nguyenthanhvinh31305@gmail.com>
2024-12-06 17:00:49 +0700

    Init: Initial project

---
+++ src/main/java/vinhnguyen/ptithcm/banking/BankingApplication.java
@@ -0,0 +1,14 @@
+package vinhnguyen.ptithcm.banking;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
+
+@SpringBootApplication
+public class BankingApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BankingApplication.class, args);
+    }
+
+}
